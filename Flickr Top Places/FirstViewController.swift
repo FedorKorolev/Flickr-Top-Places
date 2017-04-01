a8e6fc9d5b9509428b9be520bb3c162247bb6147
FedorKorolev <korolev.f@icloud.com>
2017-04-01 21:33:07 +0300

    Initial Commit

---
+++ Flickr Top Places/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Flickr Top Places
+//
+//  Created by Фёдор Королёв on 01.04.17.
+//  Copyright © 2017 Фёдор Королёв. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
