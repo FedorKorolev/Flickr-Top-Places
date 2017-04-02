f06e91ad2f6f30b447980e039cc2316daa4163ce
FedorKorolev <korolev.f@icloud.com>
2017-04-03 00:12:41 +0300

    Photo Titles and Large Icons

--- Flickr Top Places/PhotosTableViewController.swift
+++ Flickr Top Places/PhotosTableViewController.swift
@@ -17,6 +17,8 @@ class PhotosTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = place?.name
+        
         let api = FlickrAPIService()
         api.search(place: (place?.id)!,
                    success: { photos in
@@ -40,14 +42,20 @@ class PhotosTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return photos.count
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
+        
+        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         image.loadImage(link: photos[indexPath.row].iconLink)
         print("Loaded image: \(image)")
-        
         cell.addSubview(image)
+        
+        cell.textLabel?.text = photos[indexPath.row].title
 
         return cell
     }
